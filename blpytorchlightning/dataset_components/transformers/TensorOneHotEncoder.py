f85448cdb1a60856887f9ffdfde6879e3361a647
Nathan <njneeteson@gmail.com>
2022-11-20 14:56:43 -0700

    add a rotation transformer for data augmentation

--- blpytorchlightning/dataset_components/transformers/TensorOneHotEncoder.py
+++ blpytorchlightning/dataset_components/transformers/TensorOneHotEncoder.py
@@ -9,15 +9,34 @@ from blpytorchlightning.dataset_components.transformers.BaseTransformer import (
 
 class TensorOneHotEncoder(BaseTransformer):
 
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int) -> None:
+        """
+        Initialization method.
+
+        Parameters
+        ----------
+        num_classes : int
+            How many classes will be in the targets.
+        """
         self.num_classes = num_classes
 
     def __call__(
             self, sample: tuple[torch.Tensor, torch.Tensor]
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        A magic method that allows this function to be called as a function. Pass the sample through, rescale
+        the image and convert everything to tensors.
 
-        image, masks = sample
-        masks = torch.movedim(torch.nn.functional.one_hot(masks, num_classes=self.num_classes), -1, 0)
-        return image, masks
+        Parameters
+        ----------
+        sample: tuple[torch.Tensor, torch.Tensor]
+            The input sample.
 
-
+        Returns
+        -------
+        tuple[torch.Tensor, torch.Tensor]
+            The transformed sample.
+        """
+        image, targets = sample
+        targets = torch.movedim(torch.nn.functional.one_hot(targets, num_classes=self.num_classes), -1, 0)
+        return image, targets
